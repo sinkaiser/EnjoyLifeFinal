b7c23482cca3780970bdd4eddbfe46b3eb98ec1c
Peterchenkuan <peter77613@gmail.com>
2015-09-28 13:13:02 +0800

    add logout fix controller
    controller/CheckMemberId
    add includes logout

--- EnjoyLife/src/com/member/controller/CheckMemberId.java
+++ EnjoyLife/src/com/member/controller/CheckMemberId.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.member.model.MemberService;
 
-@WebServlet("/display/checkMemberId")
+@WebServlet("/checkMemberId")
 public class CheckMemberId extends HttpServlet {
 	private static final long serialVersionUID = 1L;       
     public CheckMemberId() {
