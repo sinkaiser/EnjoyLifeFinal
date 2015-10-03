8312b27d55cead021e2fac7e0b8bcc5d507bc738
Student07 <Student8007@III-EEIT>
2015-10-03 16:45:31 +0800

    kaijie bloglist add star score

--- EnjoyLife/src/com/blog/controller/BlogScoreServlet.java
+++ EnjoyLife/src/com/blog/controller/BlogScoreServlet.java
@@ -7,6 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Session;
+import org.json.simple.JSONValue;
+
+import com.blog.model.BlogDAO;
+import com.blog.model.Hibernate.BlogDAOHibernate;
+import com.util.HibernateUtil;
+
 @WebServlet("/BlogScoreServlet")
 public class BlogScoreServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,9 +24,20 @@ public class BlogScoreServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String score = request.getParameter("rating");
-		System.out.println(score);
+		String ArticleNo = request.getParameter("ArticleNo");
+		String score = request.getParameter("score");
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		
+		session.beginTransaction();
+		BlogDAO dao = new BlogDAOHibernate(session);
+		boolean result = dao.updateAvgScore(Integer.parseInt(score), ArticleNo);
+		if(!result){
+			session.getTransaction().rollback();
+		}
+		session.getTransaction().commit();
+		response.setContentType("text/html;charset=UTF-8");
+		String jsonString = JSONValue.toJSONString(result);
+		response.getWriter().write(jsonString);
 	}
 
 	
