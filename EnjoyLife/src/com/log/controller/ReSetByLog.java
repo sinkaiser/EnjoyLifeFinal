35d529649f41edf0c380fa1aa4e07d797515b92b
usodaaaaa <super@super-PC>
2015-10-06 06:29:48 +0800

    add reset

--- EnjoyLife/src/com/log/controller/ReSetByLog.java
+++ EnjoyLife/src/com/log/controller/ReSetByLog.java
@@ -1,11 +1,20 @@
 package com.log.controller;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.log.model.AdminLogBean;
 
 /**
  * Servlet implementation class ReSetByLog
@@ -28,7 +37,39 @@ public class ReSetByLog extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String time=request.getParameter("time");
+		String time2=request.getParameter("time2");
 		
+		Date date;
+		Date date2;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		
+		try {
+			date=sdf.parse(time);
+			date2=sdf.parse(time);
+		} catch (ParseException e) {
+			date=null;
+		}
+		
+		HttpSession session=request.getSession();
+		Map<String,List<AdminLogBean>> map=(Map<String,List<AdminLogBean>>)session.getAttribute("AdminLog");
+		
+		
+//		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+//		Date Logdate=new Date();
+//		String d=sdf2.format(date);
+		
+		if(date.getTime()<new Date().getTime()){
+			for(int p=1;p>7;p++)
+			
+			for(AdminLogBean bean:map.get("day"+p)){
+//				if(date.getTime()<bean.getLogDate().getTime()&&
+					
+					
+				
+			}
+			
+		}
+
 		
 		
 	}
